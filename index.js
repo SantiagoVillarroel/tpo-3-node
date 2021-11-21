566247b3425180521d09a9ee34d72b83ab927202
Santiago Villarroel <santigibsonsg@gmail.com>
2021-11-21 00:03:02 -0300

    agrego routes para archivos html

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+inicio = __dirname + '/inicio.html';
+historico = __dirname + '/historico.html';
+quienesSomos = __dirname + '/quienesSomos.html';
 
 app.use(express.static('public'));
+
+//var public = path.join(__dirname, 'public');
+
+app.get('/inicio', function(req, res) {
+    res.sendFile(inicio);
+});
+app.get('/historico', function(req, res) {
+    res.sendFile(historico);
+});
+app.get('/quienesSomos', function(req, res) {
+    res.sendFile(quienesSomos);
+});
 
 var things = require('./things.js');
 
