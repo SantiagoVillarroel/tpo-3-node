40edd047daa746eb5e048955e6a03ffc26c2c41e
Santiago Villarroel <santigibsonsg@gmail.com>
2021-11-28 23:56:46 -0300

    cambio de nombres y comentarios

--- api.js
+++ api.js
@@ -2,38 +2,31 @@ const { request } = require('express');
 var express = require('express');
 var router = express.Router();
 
-let clase = require('./clase.js');
+let manipulacionDatos = require('./manipulacionDatos.js');
 
 var bodyParser = require('body-parser')
- 
-// create application/json parser
+
 var jsonParser = bodyParser.json()
- 
-//var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-const datosInicio =  require('./datos.json')
-
-const indiceDeTiposDeDolarValorHistorico= require('./historico_JSON/indiceValoresHistoricosTipoDolar.json');
 
  router.get('/ValorTiposDeDolarHoy', function(req, res) {
-   res.send(clase.obtenerDatosInicio());
-   clase.prueba();
+   res.send(manipulacionDatos.obtenerDatosPaginaInicio());
 });
 
 router.post('/ValorTiposDeDolarHoy', jsonParser, (req, res) => {
    console.log(req.body);
    const {id, nombre, venta, compra} = req.body;
+   //Validación de datos
    if(!isNaN(id) && typeof(nombre)==='string' && !isNaN(venta) && !isNaN(compra) && venta>=0 && compra>=0){
-      res.send(clase.crearDatoInicio(id, nombre, venta, compra));
+      res.send(manipulacionDatos.crearDatoPaginaInicio(id, nombre, venta, compra));
    }else{
       res.sendStatus(400);
    }
 });
 
-router.put('/ValorTiposDeDolarHoy', function(req, res) {
+router.put('/ValorTiposDeDolarHoy', jsonParser, function(req, res) {
    const {id, nombre, venta, compra} = req.body;
    if(!isNaN(id) && typeof(nombre)==='string' && !isNaN(venta) && !isNaN(compra) && venta>=0 && compra>=0){
-      let obj = clase.actualizarDatoInicio(id, nombre, venta, compra);
+      let obj = manipulacionDatos.actualizarDatoPaginaInicio(id, nombre, venta, compra);
       if(obj != null){
          res.send(obj);
       }else{
@@ -45,17 +38,17 @@ router.put('/ValorTiposDeDolarHoy', function(req, res) {
 });
 
 router.get('/ValoresHistoricosDolar', function(req, res){
-   //dolares?tipo=blue&cantidad=10&desde=0
    //librerias para validar - joi
+   const tipo = req.query.tipo;
    const cantidad=req.query.cantidad;
    const desde=req.query.desde;
-   res.send((datosHistorico[req.params.id]).slice[desde, desde+cantidad]);
+   res.send(manipulacionDatos.obtenerHistoricoCantidadDesde(tipo, cantidad, desde));
 });
 
 router.get('/ValoresHistoricosDolar/:NombreTipoDeDolar', function(req, res){
    let nombre = req.params.NombreTipoDeDolar;
    if(typeof(nombre) === 'string'){
-      res.send(clase.obtenerNombreArchivoHistorico(nombre));
+      res.send(manipulacionDatos.obtenerArchivoHistorico(nombre));
    }else{
       res.sendStatus(400);
    }
---
+++ manipulacionDatos.js
@@ -0,0 +1,50 @@
+const datosPaginaInicio =  require('./datosPaginaInicio.json')
+
+const indiceDeTiposDeDolarValorHistorico= require('./historico_JSON/indiceValoresHistoricosTipoDolar.json');
+
+exports.obtenerDatosPaginaInicio = function obtenerDatosPaginaInicio(){
+    //Devuelve JSON correspondiente
+    return datosPaginaInicio;
+}
+
+exports.crearDatoPaginaInicio = function crearDatoPaginaInicio(id, nombre, venta, compra){
+    //Crea objeto a ser insertado
+    let objeto = {
+        "id":id,
+        "nombre":nombre,
+        "compra":compra,
+        "venta":venta
+     };
+     datosPaginaInicio.push(objeto); //almacenar fecha de guardado?
+     return objeto; //Devuelve objeto para res.send() en api
+}
+
+exports.actualizarDatoPaginaInicio = function actualizarDatoPaginaInicio(id, nombre, venta, compra){
+    //Busco indice de objeto correspondiente a id
+    const indiceEncontrado = datosPaginaInicio.findIndex(elem => elem.id===id);
+    let res;
+    if(!isNaN(indiceEncontrado)){
+        //Actualizo datos
+       datosPaginaInicio[indiceEncontrado].nombre=nombre;
+       datosPaginaInicio[indiceEncontrado].compra=compra;
+       datosPaginaInicio[indiceEncontrado].venta=venta;
+       res = datosPaginaInicio[indiceEncontrado];
+    }else{
+        res=null;
+    }
+    return res;
+}
+
+exports.obtenerArchivoHistorico = function obtenerArchivoHistorico(nombreDeTipoDolar){
+    //Busco tipo de dólar en indice
+    let tipo = indiceDeTiposDeDolarValorHistorico.find(element => {
+       return element.tipoDolar === nombreDeTipoDolar;
+     });
+    nombreArchivoHistorico = tipo.nombreArchivo; //Obtengo nombre del archivo JSON correspondiente al tipo de dólar
+    return require('./historico_JSON/'+nombreArchivoHistorico);
+}
+
+exports.obtenerHistoricoCantidadDesde = function obtenerHistoricoCantidadDesde(tipo, cantidad, desde){
+    const archivoHistorico = obtenerArchivoHistorico(tipo);
+    return archivoHistorico.slice[desde, desde+cantidad];
+}
