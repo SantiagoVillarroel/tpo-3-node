27ec29147a565946b20e9fc71fd10bcd0a98ee5b
Santiago Villarroel <santigibsonsg@gmail.com>
2022-02-04 20:56:21 -0300

    un comment (hay que hacer server https)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var https = require('https')
 var app = express();
 
 app.use(express.static('public'));
@@ -10,5 +11,6 @@ app.use('/api', api);
 const port = process.env.PORT || 3000;
 
 app.listen(port,()=>console.log("Server on Port ",port));
+//https.createServer(options, app).listen(port)
 
 //const y let, no var
