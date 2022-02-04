be75f4642a41cb03fbd6de077eb73ef748175dd1
limon <daniel.campos@est.fi.uncoma.edu.ar>
2022-02-04 16:02:30 -0300

    04022022

--- public/datos.js
+++ public/datos.js
@@ -5,8 +5,9 @@ const tabla=document.querySelector("#tablaPrecios tbody");
 function getDatos(){
     fetch('http://localhost:3000/api/valorTiposDeDolarHoy')
     .then(respuesta => respuesta.json()) //indicamos el formato en el que se desea obtener la informacion
-    .then(objeto =>
-        objeto.forEach(dolar=>{
+    .then(objeto =>{
+        console.log(objeto.message);
+        objeto.message.forEach(dolar=>{
             const row= document.createElement('tr');
             row.classList.add('border-bottom','border-3','boder-'); //Para estilos (bootstrap)
             //Agrego info a innerHTML
@@ -16,7 +17,7 @@ function getDatos(){
                 <td>$${dolar.compra}</td>
             `;
             tabla.appendChild(row); //Concateno nueva fila
-        })    
+        }) }   
     );
 }
 
